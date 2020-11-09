3ce48b9dfa36932f2c95d2bdcb92d7988ddedda0
Joe <joseph@teloconsolutions.com>
2020-11-08 20:20:28 -0800

    updates to user.js and item.js, including associations

--- models/user.js
+++ models/user.js
@@ -29,7 +29,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     },
     state: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(2),
       allowNull: false
     },
     zip: {
@@ -55,6 +55,12 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     }
   });
+
+  User.associate = function(models) {
+    User.hasMany(models.User, {
+      onDelete: "cascade"
+    });
+  };
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
