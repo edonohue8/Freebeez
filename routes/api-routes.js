f15e484e8409e20f0d93c2de943454bec42ad4d4
Jorge Martinez <j.a.mjorgemartinez21@gmail.com>
2020-11-10 20:07:30 -0800

    Uploaded routes and authentication

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -41,7 +41,7 @@ module.exports = function(app) {
 
   // Route for logging user out
   app.get("/logout", (req, res) => {
-    req.logout();
+    // will not work because logout doesnt exist req.logout();
     res.redirect("/");
   });
 
@@ -61,6 +61,8 @@ module.exports = function(app) {
   });
 
   // Need route to delete user WITH items associated with user
+// this code alone shall be able to delte user and everything associated with user like user items
+
   app.delete("api/user_data/:id", (req, res) => {
     db.User.destroy({
       include: db.Item,
@@ -141,6 +143,7 @@ module.exports = function(app) {
   });
 
   // Need route to delete item
+  //might not need this due to line 66 being responsible for deleting all of user and everything associated with user
   app.delete("api/item_data/:id", (req, res) => {
     db.Item.destroy({
       where: {
@@ -151,3 +154,41 @@ module.exports = function(app) {
     });
   });
 };
+
+
+// Load in our dependencies
+var express = require('express');
+var jwt = require('jsonwebtoken');
+
+var app = express();
+
+// Register the home route that displays a welcome message
+// This route can be accessed without a token
+app.get('/', function(req, res){
+  res.send("Welcome to our API");
+})
+
+// Register the route to get a new token
+// In a real world scenario we would authenticate user credentials
+// before creating a token, but for simplicity accessing this route
+// will generate a new token that is valid for 2 minutes
+app.get('/token', function(req, res){
+  var token = jwt.sign({username:"ado"}, 'supersecret',{expiresIn: 120});
+  res.send(token)
+})
+
+// Register a route that requires a valid token to view data
+/*app.get('/api', function(req, res){
+  var token = req.query.token;
+  jwt.verify(token, 'supersecret', function(err, decoded){
+    if(!err){
+      var secrets = {"" : "","" : "","" : ""};
+      res.json(secrets);
+    } else {
+      res.send(err);
+    }
+  })
+})
+
+// Launch our app on port ""
+app.listen('');*/
