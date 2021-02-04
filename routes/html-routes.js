8360fe553b45de2aed429d40f7b51a2f789031f2
Nolanws1 <Nolanws@gmail.com>
2021-02-04 12:19:29 -0800

    cleaning code

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,36 +4,15 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app) {
   app.get("/members", (req, res) => {
     res.render("partials/members");
-    // console.log(res);
+    
   });
 
   app.get("/", (req, res) => {
-    // if (req.user) {
-    //   res.redirect("/members");
-    // }
+    
     res.render("layouts/main");
-    // console.log(res);
+    
   });
-  // app.get("/", (req, res) => {
-  //   // If the user already has an account send them to the members page
-  //   if (req.user) {
-  //     res.redirect("/members");
-  //   }
-  //   res.sendFile(path.join(__dirname, "../public/login.html"));
-  // });
-  // app.get("/", (req, res) => {
-  //   // If the user already has an account send them to the members page
-  //   if (req.user) {
-  //     res.redirect("/members");
-  //   }
-  //   res.render("main", { title: Login });
-  // });
-
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  // app.get("/members", isAuthenticated, (_req, res) => {
-  //   res.sendFile(path.join(__dirname, "../public/members.html"));
-  // });
+ 
   app.get("/members", isAuthenticated, (_req, res) => {
     res.render("partials/members");
   });
